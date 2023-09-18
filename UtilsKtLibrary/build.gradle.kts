d66d4eb512f9aaf100573a39ff74acfa93c93064
Jorge Agulló <agullojorge@gmail.com>
2023-09-18 07:35:10 +0200

    Se añade el id de mave-publish y el grupo

--- UtilsKtLibrary/build.gradle.kts
+++ UtilsKtLibrary/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -41,3 +42,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+group = "com.github.JorgeAgulloM"
