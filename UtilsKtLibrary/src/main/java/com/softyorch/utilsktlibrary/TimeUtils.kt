873f1765d4f01d7646c46c327b3cd4865f2275dd
Jorge1 <agullojorge@gmail.com>
2023-09-21 20:24:33 +0200

    Add comments in TimeUtils functions

--- UtilsKtLibrary/src/main/java/com/softyorch/utilsktlibrary/TimeUtils.kt
+++ UtilsKtLibrary/src/main/java/com/softyorch/utilsktlibrary/TimeUtils.kt
@@ -8,6 +8,15 @@ object TimeUtils {
 
     private val monthListValues = listOf(0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
 
+    /**
+     * Transforms the date of a calendar to ISO8601 format without milliseconds and timezone
+     * by returning the result in a String.
+     *
+     * Calendar -> String
+     *
+     * In case of error, it will return a string with the same format as expected but with the
+     * values set to 0.
+     */
     fun Calendar.toDateFormattedISO8601(): String = try {
         val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
         val currentDate = sdf.format(this.time)
@@ -17,6 +26,14 @@ object TimeUtils {
         "0000-00-00T00:00:00"
     }
 
+    /**
+     * Returns a String of the date of a Calendar.
+     *
+     * Calendar -> String
+     *
+     * In case of error, it will return a string with the same format as expected but with the
+     * values set to 0.
+     */
     fun Calendar.getDateOfNow(): String = try {
         val year = this.get(Calendar.YEAR)
         val month = this.get(Calendar.MONTH)
@@ -27,6 +44,14 @@ object TimeUtils {
         "0000-00-00"
     }
 
+    /**
+     * Returns a String of the time of a Calendar.
+     *
+     * Calendar -> String
+     *
+     * In case of error, it will return a string with the same format as expected but with the
+     * values set to 0.
+     */
     fun Calendar.getCurrentTimeOfNow(): String = try {
         val hour = this.get(Calendar.HOUR)
         val minutes = this.get(Calendar.MINUTE)
@@ -37,6 +62,13 @@ object TimeUtils {
         "00:00:00"
     }
 
+    /**
+     * Transforms a full String date into a Calendar. No need to pass seconds.
+     *
+     * String -> Calendar
+     *
+     * On error, will return a Calendar.getInstance() value.
+     */
     fun String.toCalendarTime(): Calendar = try {
         Calendar.getInstance().apply {
             val noSeconds = -1
@@ -59,6 +91,14 @@ object TimeUtils {
         Calendar.getInstance()
     }
 
+    /**
+     * Returns from an ISO8601 String dateTime value, the date value in String type.
+     *
+     * String -> String
+     *
+     * In case of error, it will return a string with the same format as expected but with the
+     * values set to 0.
+     */
     fun String.getDateOfNow(): String = try {
         this.split("T")[0]
     } catch (e: Exception) {
@@ -66,6 +106,14 @@ object TimeUtils {
         "0000-00-00"
     }
 
+    /**
+     * Returns from an ISO8601 String dateTime value, the time value in String type.
+     *
+     * String -> String
+     *
+     * In case of error, it will return a string with the same format as expected but with the
+     * values set to 0.
+     */
     fun String.getCurrentTimeOfNow(): String = try {
         this.split("T")[1]
     } catch (e: Exception) {
@@ -73,12 +121,17 @@ object TimeUtils {
         "00:00:00"
     }
 
+    /**
+     * Removes seconds from a String time offset.
+     *
+     * String 12:23:34 -> String 12:23
+     *
+     * */
     fun String.deleteSeconds(): String = try {
         this.substringBeforeLast(":")
     } catch (e: Exception) {
         e.printStackTrace()
         this
     }
-
 
 }
