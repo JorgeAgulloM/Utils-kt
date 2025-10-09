6f6f17eb7a8d4085a5f61e723924f833b5ca7f6c
Jorge1 <agullojorge@gmail.com>
2025-10-09 14:33:42 +0200

    Add core, appcompat, and material dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,5 +43,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
